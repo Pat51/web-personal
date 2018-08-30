6cdfba198b7094435107f4441394d0bd50f1c6b4
Daniel <danielmandarano@yahoo.com.ar>
2018-08-29 21:04:30 -0300

    First commit

---
+++ webpersonal/core/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, HttpResponse
+
+html_base = """
+
+
+"""
+
+
+def home(request):
+    return render(request, "core/home.html")
+
+
+def about(request):
+    return render(request, "core/about.html")
+
+
+
+
+
+def contact(request):
+    return render(request, "core/contact.html")
